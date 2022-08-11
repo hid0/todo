1fcf22b7a25edfcd538e75477a177de01c30884e
hid0 <faizhid11@gmail.com>
2022-08-12 00:00:18 +0700

    fix cannot run, then success all function

--- js/script.js
+++ js/script.js
@@ -39,10 +39,15 @@ document.addEventListener(RENDER_EVENT, function () {
   const uncompletedTODOList = document.getElementById("todos");
   uncompletedTODOList.innerHTML = "";
 
+  const completedTODOList = document.getElementById("completed-todos");
+  completedTODOList.innerHTML = "";
+
   for (const todoItem of todos) {
     const todoElement = makeTodo(todoItem);
     if (!todoItem.isCompleted) {
       uncompletedTODOList.append(todoElement);
+    } else {
+      completedTODOList.append(todoElement);
     }
   }
 });
@@ -105,9 +110,37 @@ function addTaskToCompleted(todoId) {
 
 function findTodo(todoId) {
   for (const todoItem of todos) {
-    if (todoItem === todoId) {
+    if (todoItem.id === todoId) {
       return todoItem;
     }
   }
   return null;
+}
+
+function removeTaskFromCompleted(todoId) {
+  const todoTarget = findTodoIndex(todoId);
+
+  if (todoTarget === -1) return;
+
+  todos.splice(todoTarget, 1);
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function undoTaskFromCompleted(todoId) {
+  const todoTarget = findTodo(todoId);
+
+  if (todoTarget == null) return;
+
+  todoTarget.isCompleted = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function findTodoIndex(todoId) {
+  for (const index in todos) {
+    if (todos[index].id === todoId) {
+      return index;
+    }
+  }
+
+  return -1;
 }
