f8d0d77f8d9108b1335bc6b0d0d8c241b089db92
hid0 <faizhid11@gmail.com>
2022-08-17 22:32:12 +0700

    finished all✅

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const todos = [];
 const RENDER_EVENT = "render-todo";
+const SAVED_EVENT = "saved-todo";
+const STORAGE_KEY = "TODO_APPS";
 
 document.addEventListener("DOMContentLoaded", function () {
   const submitForm = document.getElementById("form");
@@ -8,6 +10,9 @@ document.addEventListener("DOMContentLoaded", function () {
     event.preventDefault();
     addTodo();
   });
+  if (isStorageExist()) {
+    loadDataFromStorage();
+  }
 });
 
 function addTodo() {
@@ -153,15 +158,12 @@ function findTodoIndex(todoId) {
 }
 
 function saveData() {
-  if (isStorageExist) {
+  if (isStorageExist()) {
     const parsed = JSON.stringify(todos);
-    localStorage.setItem("STORAGE_KEY", parsed);
-    document.dispatchEvent(new Event(RENDER_EVENT));
+    localStorage.setItem(STORAGE_KEY, parsed);
+    document.dispatchEvent(new Event(SAVED_EVENT));
   }
 }
-
-const SAVED_EVENT = "saved-todo";
-const STORAGE_KEY = "TODO_APPS";
 
 function isStorageExist() {
   if (typeof Storage === undefined) {
@@ -173,4 +175,18 @@ function isStorageExist() {
 
 document.addEventListener(SAVED_EVENT, function () {
   console.log(localStorage.getItem(STORAGE_KEY));
+  // alert("berhasil!");
 });
+
+function loadDataFromStorage() {
+  const serializedData = localStorage.getItem(STORAGE_KEY);
+  let data = JSON.parse(serializedData);
+
+  if (data !== null) {
+    for (const todo of data) {
+      todos.push(todo);
+    }
+  }
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
